77a742366ab676daacdd8f2d9122370c33ceba3d
Che-Cheng-Chang <r06921002@ntu.edu.tw>
2018-05-22 18:58:13 +0800

    second commit hw5_test.sh

--- hw5/hw5_test.sh
+++ hw5/hw5_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://www.dropbox.com/s/9i4t3f3niis7n1j/model1.hdf5 -P ./model/
-wget https://www.dropbox.com/s/nf6tz73gjo6jjff/model2.hdf5 -P ./model/
+wget https://www.dropbox.com/s/9i4t3f3niis7n1j/model1.hdf5
+wget https://www.dropbox.com/s/nf6tz73gjo6jjff/model2.hdf5
 python3 hw5_test.py $1 $2 $3
